7ffc644c56dbb2dd00c8401a2164de323f9ed680
sungjin.bae <baehun92@naver.com>
2020-04-08 02:20:20 +0900

    Windows 프로젝트 생성 스크립트 수정.
    프로젝트에는 vcpkg, vulkan, glfw 가 필요한데
    설치 및 환경 설정을 하는 기능 추가.
    
    참고.
    Windows 환경에서는 vcpkg 를 통해 패키지를 관리.
    vulkan 은 vulkan 홈페이지에서 인스톨러를 받아야한다.
    vcpkg 은 vulkan path 를 통해 프로젝트의 환경을 설정해준다.)

--- engine/build.bat
+++ engine/build.bat
@@ -1,15 +1,37 @@
 @echo off
 echo Start build BEngine project
-SET PLATFORM="windows"
+set PLATFORM="windows"
 
+set CURRENT_DIR=%cd%
+set VCPKG_PATH=%VCPKG_PATH%
+
+echo %VCPKG_PATH%
 echo Make %PLATFORM% proejct
 
-if exist %pwd%/%PLATFORM%/ (
-  echo clean %PLATFORM% dir
-  rmdir %PLATFORM%
+if "%VCPKG_PATH%" == "" (
+  echo "Install vcpkg"
+  git clone "https://github.com/Microsoft/vcpkg.git"
+  cd vcpkg
+  call bootstrap-vcpkg.bat
+  
+  echo "Install glfw3 package"
+  call vcpkg install glfw3:x64-windows
+
+  echo "Install vulkan package"
+  call vcpkg install vulkan:x64-windows
+
+  setx VCPKG_PATH %cd%
+  echo "Finished install vcpkg. Please new cmd open and restart build.bat file"
 )
 
-mkdir %PLATFORM%
+if not exist %pwd%/%PLATFORM%/ (
+  mkdir %PLATFORM%
+)
+
+echo %cd%
+set TOOLCHAIN_PATH="%VCPKG_PATH%\scripts\buildsystems\vcpkg.cmake"
 cd %PLATFORM%
-cmake -G "Visual Studio 15 2017 Win64" ..
+cmake -G "Visual Studio 15 2017 Win64" ^
+    -DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN_PATH% ^
+    ..
 cd ..
